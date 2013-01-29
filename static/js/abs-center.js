e85a649eb82013db09c1f609f915620f3bcf015e
Matthew Lewis <matt@mplewis.com>
2013-01-29 17:03:56 -0600

    figured out the sub-pixel fix

--- static/js/abs-center.js
+++ static/js/abs-center.js
@@ -3,10 +3,16 @@
 $(document).ready(function(){
 	$(window).resize(function(){
 		items = $('#loginmaster');
+		// This modulo business fixes a problem with sub-pixel rendering: when
+		// the window is an odd size, the Log In button doesn't line up with the
+		// text boxes. This forces the window measured height to always be even.
+		var topPos = ($(window).height() - $('.abs-center').outerHeight()) / 2;
+		var topModulo = topPos % 2;
+		var topPosFix = topPos - topModulo;
 		items.css({
 			position:'absolute',
 			left: ($(window).width() - $('.abs-center').outerWidth()) / 2,
-			top: ($(window).height() - $('.abs-center').outerHeight()) / 2
+			top: topPosFix
 		});
 		// fade in login box once it's rendered
 		items.fadeIn('slow');
