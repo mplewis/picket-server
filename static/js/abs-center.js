8198636bd840deea0fe703888473f7ec562ea475
Matthew Lewis <matt@mplewis.com>
2013-01-29 22:20:40 -0600

    FINALLY FIXED THE ONE PIXEL GLITCH. This will not hold, I know it

--- static/js/abs-center.js
+++ static/js/abs-center.js
@@ -1,14 +1,16 @@
 // Center the login box vertically and horizontally on the page
 // Don't center on page load, offload to Google Web Fonts script
 $(document).ready(function(){
-	$(window).resize(function(){
-		items = $('#loginmaster');
-		items.css({
-			position:'absolute',
-			left: ($(window).width() - $('.abs-center').outerWidth()) / 2,
-			top: ($(window).height() - $('.abs-center').outerHeight()) / 2
-		});
-		// fade in login box once it's rendered
-		items.fadeIn('slow');
-	});
+    $(window).resize(function(){
+        items = $('#loginmaster');
+        var topTemp = ($(window).height() - $('.abs-center').outerHeight()) / 2;
+        topTemp = topTemp - topTemp % 2;
+        items.css({
+            position:'absolute',
+            left: ($(window).width() - $('.abs-center').outerWidth()) / 2,
+            top: topTemp
+        });
+        // fade in login box once it's rendered
+        items.fadeIn('slow');
+    });
 });
