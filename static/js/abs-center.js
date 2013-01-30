304d11a444af0ee2cff4296c909a14ccae987bca
Matthew Lewis <matt@mplewis.com>
2013-01-29 22:00:22 -0600

    turns out the modulo fix doesn't really fix anything. FIXME: fix the fucking login 1px bug. maybe it only happens in chrome?

--- static/js/abs-center.js
+++ static/js/abs-center.js
@@ -3,16 +3,10 @@
 $(document).ready(function(){
 	$(window).resize(function(){
 		items = $('#loginmaster');
-		// This modulo business fixes a problem with sub-pixel rendering: when
-		// the window is an odd size, the Log In button doesn't line up with the
-		// text boxes. This forces the window measured height to always be even.
-		var topPos = ($(window).height() - $('.abs-center').outerHeight()) / 2;
-		var topModulo = topPos % 2;
-		var topPosFix = topPos - topModulo;
 		items.css({
 			position:'absolute',
 			left: ($(window).width() - $('.abs-center').outerWidth()) / 2,
-			top: topPosFix
+			top: ($(window).height() - $('.abs-center').outerHeight()) / 2;
 		});
 		// fade in login box once it's rendered
 		items.fadeIn('slow');
