db001985515d19e57daa0044715596373acaa5c8
Matthew Lewis <matt@mplewis.com>
2013-01-30 16:54:54 -0600

    prior state

--- static/js/abs-center.js
+++ static/js/abs-center.js
@@ -2,15 +2,15 @@
 // Don't center on page load, offload to Google Web Fonts script
 $(document).ready(function(){
     $(window).resize(function(){
-        items = $('#loginmaster');
+        var toShow = $('.login');
         var topTemp = ($(window).height() - $('.abs-center').outerHeight()) / 2;
         topTemp = topTemp - topTemp % 2;
-        items.css({
+        toShow.css({
             position:'absolute',
             left: ($(window).width() - $('.abs-center').outerWidth()) / 2,
             top: topTemp
         });
         // fade in login box once it's rendered
-        items.fadeIn('slow');
+        toShow.fadeIn('slow');
     });
 });
